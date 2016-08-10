3e9282d90444b0418cb88c391afd4c0f1630ce4b
McSwaggens <McSwaggens@protonmail.com>
2016-08-11 01:26:37 +1000

    Fixed GPU methods for PASM invocation

--- Pavic/GPU.cs
+++ Pavic/GPU.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using PASM;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -33,62 +34,74 @@ namespace Pavic
 			screen.Closed += main.OnClosed;
 		}
 		
-		public static void Push (int x, int y)
+		public static void Push (Engine engine, byte[] x, byte[] y)
 		{
-			gpu.vertexStack.Add (new Vertex (new Vector2f(x, y)));
+			gpu.vertexStack.Add (new Vertex (new Vector2f(x[0], y[0])));
 		}
 		
-		public static void Pop ()
+		public static void PushC (Engine engine, byte[] r, byte[] g, byte[] b)
+		{
+			Vertex v = gpu.vertexStack[gpu.vertexStack.Count];
+			v.Color = new Color (r[0], g[0], b[0]);
+		}
+		
+		public static void PushC_A (Engine engine, byte r, byte g, byte b, byte a)
+		{
+			Vertex v = gpu.vertexStack[gpu.vertexStack.Count];
+			v.Color = new Color (r, g, b, a);
+		}
+		
+		public static void Pop (Engine engine)
 		{
 			gpu.vertexStack.RemoveAt(gpu.vertexStack.Count);
 		}
 		
-		public static void Burst ()
+		public static void Burst (Engine engine)
 		{
 			gpu.vertexStack.Clear();
 		}
 		
-		public static void Clear ()
+		public static void Clear (Engine engine)
 		{
 			gpu.screen.Clear ();
 		}
 		
-		public static void DrawQuad ()
+		public static void DrawQuad (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.Quads);
 		}
 		
-		public static void DrawPoint ()
+		public static void DrawPoint (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.Points);
 		}
 		
-		public static void DrawLine ()
+		public static void DrawLine (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.Lines);
 		}
 		
-		public static void DrawStrip ()
+		public static void DrawStrip (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.LinesStrip);
 		}
 		
-		public static void DrawTriangle ()
+		public static void DrawTriangle (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.Triangles);
 		}
 		
-		public static void DrawTriangleFan ()
+		public static void DrawTriangleFan (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.TrianglesFan);
 		}
 		
-		public static void DrawTriangleStrip ()
+		public static void DrawTriangleStrip (Engine engine)
 		{
 			gpu.screen.Draw (gpu.vertexStack.ToArray(), PrimitiveType.TrianglesStrip);
 		}
 		
-		public static void Draw ()
+		public static void Draw (Engine engine)
 		{
 			gpu.screen.Display ();
 		}
